72dfc7aeec0285433f0b9810bdf2b4fb80c3c202
nathan.paiva <nathan.paiva@academico.ufpb.br>
2024-11-20 18:08:26 -0300

    Rscript_quase_ok

--- Geneland/Scripts/Geneland_analysis/Geneland_Cnemis.R
+++ Geneland/Scripts/Geneland_analysis/Geneland_Cnemis.R
@@ -18,21 +18,28 @@ coord <- as.matrix(coord.temp)
 mt.temp <- read.table('/home/nathan/Documents/Doutorado_diversidade_genética/Phylogeogra/Geneland/Scripts/Geneland_analysis/filtered_mitochondrial_data_no_header.txt', na.string='NA')
 mt <- as.matrix(mt.temp)
 
+###se eu tiver dado morfológicos tem como usar
+#morf.temp <- read.table('morfologia.txt', na.string='NA')
+#morf <- as.matrix(morf.temp)
+
+
 # Parâmetros para a análise MCMC
 chain <- 1000000  # Número de iterações
 freq <- 1000      # Frequência de amostragem
 burnin <- 200     # Número de iterações descartadas no início
-pop <- 4          # Número máximo de populações esperadas
+pop <- 5          # Número máximo de populações esperadas
 all.freq.model <- 'Uncorrelated'  # Modelo de frequências alélicas (correlacionado ou não)
 
 # Diretório para resultados de análises sem admixagem
 noadmix.dir <- paste(maindir, 'noadmix', '/', sep="")
 
 # Cria o diretório para resultados de análises sem admixagem, se não existir
-if (!file.exists(noadmix.dir)) {
+if (file.exists(noadmix.dir)){
+  setwd(file.path(noadmix.dir))
+} else {
   dir.create(file.path(noadmix.dir))
-}
-setwd(file.path(noadmix.dir))  # Define o diretório de trabalho
+  setwd(file.path(noadmix.dir))
+} # Define o diretório de trabalho
 
 # Executa a análise MCMC sem admixagem
 MCMC(coordinates=coord,
@@ -53,6 +60,9 @@ PostProcessChain(coordinates=coord,
                  nydom=200,
                  burnin=burnin)
 
+#### plot num of pops
+Plotnpop(path.mcmc="./", burnin=200,)
+
 # Plota o número de populações inferidas
 Plotnpop(path.mcmc="./", burnin=burnin, printit=TRUE, file="Number_of_Clusters.pdf", format="pdf")
 
@@ -65,24 +75,36 @@ Fstat.output(genotypes=geno, path.mcmc="./")
 # Cria um mapa de probabilidade de pertencimento aos clusters
 PlotTessellation(coord, path.mcmc='./', printit=TRUE, path='./')
 
-# Configuração para executar múltiplas análises (loop)
-nrun <- 10        # Número de execuções
-chain <- 5000000  # Número de iterações
-freq <- 5000      # Frequência de amostragem
 
-for (irun in 1:nrun) {
-  # Define o caminho para os resultados de cada execução
-  path.mcmc <- paste(maindir, '10_Run', irun, "/", sep="")
+#######Loop for multiple runs#######################################################################
+
+maindir <- "/home/nathan/Documents/Doutorado_diversidade_genética/Phylogeogra/Geneland/Scripts/Geneland_analysis/" #### dir 
+
+
+nrun <- 10        ### number of runs
+burnin <- 200     ### burnin
+chain <- 5000000  ### number of steps in chain
+freq <-      5000 ### sampling frequency
+pop <- 5          ### max number of pops 
+
+for(irun in 1:nrun)
+{
+  ## define path to MCMC directory
+  path.mcmc <- paste(maindir,'10_Run',irun,"/",sep="")
   
-  # Cria o diretório para cada execução, se não existir
-  if (!file.exists(path.mcmc)) {
+  ### create directory
+  if (file.exists(path.mcmc)){
+    setwd(file.path(path.mcmc))
+  } else {
     dir.create(file.path(path.mcmc))
+    setwd(file.path(path.mcmc))
   }
-  setwd(file.path(path.mcmc))  # Define o diretório de trabalho
   
-  # Executa a análise MCMC
+  
+  system(paste(path.mcmc))
   MCMC(coordinates=coord,
        geno.dip.codom=geno,
+       #geno.hap=mt,
        varnpop=TRUE,
        npopmax=pop,
        spatial=TRUE,
@@ -91,39 +113,76 @@ for (irun in 1:nrun) {
        thinning=freq,
        path.mcmc=path.mcmc)
   
-  # Processa os resultados
-  PostProcessChain(coordinates=coord, path.mcmc=path.mcmc, nxdom=200, nydom=200, burnin=burnin)
+  
+  PostProcessChain(coordinates=coord,
+                   path.mcmc=path.mcmc,
+                   nxdom=200,
+                   nydom=200,
+                   burnin=burnin)
 }
 
-# Calcula a densidade posterior para cada execução
-lpd <- rep(NA, nrun)
-for (irun in 1:nrun) {
-  path.mcmc <- paste(maindir, '/', irun, "/", sep="")
-  path.lpd <- paste(path.mcmc, "log.posterior.density.txt", sep="")
+
+#### log posterior density calculation ###
+lpd <- rep(NA,nrun)
+
+for(irun in 1:nrun)
+{
+  path.mcmc <- paste(maindir,'/',irun,"/",sep="")
+  path.lpd <- paste(path.mcmc,"log.posterior.density.txt",sep="")
   lpd[irun] <- mean(scan(path.lpd)[-(1:burnin)])
 }
-order(lpd, decreasing=TRUE)  # Ordena os resultados por densidade posterior
+
+
+order(lpd,decreasing=TRUE)
 lpd
 
-# Gera gráficos para as execuções
-for (irun in 1:nrun) {
-  path.mcmc <- paste(maindir, '/', irun, "/", sep="")
+
+
+##### plot number of pops
+
+for(irun in 1:nrun)
+{
+  path.mcmc <- paste(maindir,'/',irun,"/",sep="")
   setwd(file.path(path.mcmc))
-  Plotnpop(path.mcmc=path.mcmc, burnin=burnin)
+  Plotnpop(path.mcmc=path.mcmc, burnin=200)
+}
+
+for(irun in 1:nrun)
+{
+  path.mcmc <- paste(maindir,'/',irun,"/",sep="")
+  setwd(file.path(path.mcmc))
   PosteriorMode(coordinates=coord, path.mcmc="./", file="map.pdf")
 }
 
-# Executa análises considerando admixagem
+
+
+
+
+
+
+
+###Admixture
+
+HZ(coordinates=coord,   
+   geno.dip.codom=geno,
+   path.mcmc.noadm=maindir,
+   nit=20000,
+   thinning=10,
+   path.mcmc.adm="C:/Users/Marcelo/Dropbox/Pseudis tocantins/GENELAND/nuclear/1")
+
+
 admix.dir <- "C:/Users/Marcelo/Dropbox/Pseudis tocantins/Analises/GENELAND/nuclear/admix"
 noadmix.dir <- "C:/Users/Marcelo/Dropbox/Pseudis tocantins/Analises/GENELAND/nuclear/1/"
 
-# Cria diretório para admixagem, se não existir
-if (!file.exists(admix.dir)) {
+if (file.exists(admix.dir)){
+  setwd(file.path(admix.dir))
+} else {
   dir.create(file.path(admix.dir))
+  setwd(file.path(admix.dir))
 }
-setwd(file.path(admix.dir))  # Define o diretório de trabalho
 
-# Executa análise com admixagem
+
+
 HZ(coordinates=coord,
    geno.dip.codom=geno,
    path.mcmc.noadm=noadmix.dir,
@@ -135,3 +194,4 @@ HZ(coordinates=coord,
    nit=chain,
    thinning=freq,
    path.mcmc.adm=admix.dir)
+
